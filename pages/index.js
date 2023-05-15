c35988341bac7e47510ef952b93417fd3e46ac76
Colin Sprows <colin.sprows@gmail.com>
2023-05-15 09:54:30 -0400

    lander is responsive

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,19 @@ export const Wrapper = styled.div`
 export const Container = styled.div`
 	display: flex;
 	flex-direction: row;
+	padding: 0rem 4rem;
+
+	@media (max-width: 768px) {
+		flex-direction: column;
+		padding: 0rem 0rem;
+		align-items: center;
+		justify-content: center;
+	}
+
+	@media (min-width: 1600px) {
+		justify-content: center;
+		width: 100%;
+	}
 `
 
 export const Left = styled.div`
@@ -18,13 +31,28 @@ export const Left = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	width: 60vw;
+	max-width: 800px;
+
+	@media (max-width: 768px) {
+		width: 100vw;
+		padding: 0rem 2rem;
+	}
 `
 
 export const Right = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	width: 40vw;
+	width: calc(40vw-8rem);
+	max-width: 600px;
+	
+
+	@media (max-width: 768px) {
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 `
 
 export const TextContainer = styled.div`
@@ -39,20 +67,25 @@ export const PromptPrint = styled.h4`
 	padding: ${props => props.pad || "5px 0px"};
 	border-radius: 50px;
 	font-family: InterExtraBold;
-	font-size: 1.5rem;
+	font-size: clamp(0.5rem, 4vw, 1.5rem);
 	font-weight: 100;
+
+	@media (max-width: 768px) {
+		padding: ${props => props.pad || "5px 5px"};
+	}
 `;
 
 export const Main = styled.h3`
 	font-family: InterLight;
-	font-size: 5rem;
+	font-size: clamp(2.5rem, 7vw, 5rem);
+	white-space: nowrap;
 	font-weight: 100;
 	line-height: 0.9;
 `;
 
 export const MainSerif = styled.h3`
 	font-family: DMSerifDisplayItalic;
-	font-size: 5.25rem;
+	font-size: clamp(2.5rem, 7.25vw, 5.25rem);
 	line-height: 0.85;
 	font-weight: 100;
 	text-align: center;
@@ -60,19 +93,24 @@ export const MainSerif = styled.h3`
 
 export const Sub = styled.h5`
 	font-family: InterExtraLight;
-	font-size: 1rem;
+	font-size: clamp(0.5rem, 3vw, 1rem);
 `;
 
 export const StartCreatingButton = styled.button`
     background-color: ${props => props.theme.colors.button};
     color: black;
-	padding: 1rem 6rem;
+	padding: 1rem 8vw;
 	border: 1px solid black;
 	border-radius: 50px;
 	font-family: InterBlack;
-	font-size: 2rem;
+	font-size: clamp(1.25rem, 2vw, 2rem);
 	letter-spacing: -0.05em;
 	white-space: nowrap;
+
+	@media (max-width: 768px) {
+		padding: 1rem 6rem;
+		margin-top: 1rem;
+	}
 `
 
 export default function Home() {
