bf77a9e78df7fb583dd26c4da0942d5be01dea9e
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-08-31 15:13:40 -0400

    sets up initial sign up and confirmation logic with cognito

--- pages/account/signup.js
+++ pages/account/signup.js
@@ -1,38 +1,66 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Link from 'next/link'
+import React, { useState } from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import { signUp, confirmSignUp } from "../../utils/authHandler";
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 400px;
-    margin: 6rem auto;
-`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	max-width: 400px;
+	margin: 6rem auto;
+`;
 
-const Input = styled.input`
-`
+const Input = styled.input``;
 
-const Button = styled.button`
-`
+const Button = styled.button``;
 
-const StyledLink = styled(Link)`
-`
+const StyledLink = styled(Link)``;
 
 export default function login() {
-    const [isSubmitted, setIsSubmitted] = useState(false)
-    return (
-        <Wrapper>
-            <div>Signup</div>
-            <Input type="text" placeholder="email" />
-            <Input type="password" placeholder="password" />
-            {isSubmitted &&
-                <div>
-                    <span>email sent, enter verification code below</span>
-                    <Input type="text" placeholder="verification code" />
-                </div>
-            }
-            <Button>Signup</Button>
-        </Wrapper>
-    )
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [code, setCode] = useState("");
+
+	const handleSubmit = async () => {
+		const user = await signUp(email, password);
+		console.log("signup", user);
+		setIsSubmitted(true);
+	};
+
+	const handleConfirm = async () => {
+		const response = await confirmSignUp(email, code);
+		console.log("signup", response);
+	};
+
+	return (
+		<Wrapper>
+			<div>Signup</div>
+			<Input
+				onChange={(e) => setEmail(e.target.value)}
+				type="text"
+				placeholder="email"
+				disabled={isSubmitted}
+			/>
+			<Input
+				onChange={(e) => setPassword(e.target.value)}
+				type="password"
+				placeholder="password"
+				disabled={isSubmitted}
+			/>
+			{isSubmitted && (
+				<div>
+					<span>email sent, enter verification code below</span>
+					<Input
+						type="text"
+						placeholder="verification code"
+						onChange={(e) => setCode(e.target.value)}
+					/>
+					<Button onClick={() => handleConfirm()}>Confirm</Button>
+				</div>
+			)}
+			{!isSubmitted && <Button onClick={() => handleSubmit()}>Signup</Button>}
+		</Wrapper>
+	);
 }
--- utils/authHandler.js
+++ utils/authHandler.js
@@ -1,116 +1,119 @@
-import { Auth, Hub } from 'aws-amplify';
+import { Auth, Hub } from "aws-amplify";
 
 // Signup
-export default async function signUp() {
-  try {
-    const { user } = await Auth.signUp({
-      username,
-      password,
-      attributes: {
-        email,         
-      },
-      autoSignIn: {
-        enabled: true,
-      }
-    });
-    console.log(user);
-  } catch (error) {
-    console.log('error signing up:', error);
-  }
-}
-
-//Confirmation email resend
-export default async function resendConfirmationCode() {
-    try {
-      await Auth.resendSignUp(username);
-      console.log('code resent successfully');
-    } catch (err) {
-      console.log('error resending code: ', err);
-    }
+export async function signUp(email, password) {
+	try {
+		console.log(email, password);
+		const { user } = await Auth.signUp({
+			username: email,
+			password,
+			autoSignIn: {
+				enabled: true,
+			},
+		});
+		console.log("auth", user);
+		return user;
+	} catch (error) {
+		console.log("error signing up:", error);
+		return error;
+	}
 }
 
 //confirm after receiving code
-export default async function confirmSignUp() {
-  try {
-    //doesn't allow doubled emails
-    await Auth.confirmSignUp(username, code, { forceAliasCreation: false });
-  } catch (error) {
-    console.log('error confirming sign up', error);
-  }
+export async function confirmSignUp(email, code) {
+	try {
+		const username = email;
+		//doesn't allow doubled emails
+		const response = await Auth.confirmSignUp(username, code, {
+			forceAliasCreation: false,
+		});
+		console.log("auth", response);
+		return response;
+	} catch (error) {
+		console.log("error confirming sign up", error);
+	}
+}
+
+//Confirmation email resend
+export async function resendConfirmationCode() {
+	try {
+		await Auth.resendSignUp(username);
+		console.log("code resent successfully");
+	} catch (err) {
+		console.log("error resending code: ", err);
+	}
 }
 
 //Auto sign in after sign up
-export default function listenToAutoSignInEvent() {
-    Hub.listen('auth', ({ payload }) => {
-      const { event } = payload;
-      if (event === 'autoSignIn') {
-        const user = payload.data;
-        // assign user
-      } else if (event === 'autoSignIn_failure') {
-        // redirect to sign in page
-      }
-    })
+export function listenToAutoSignInEvent() {
+	Hub.listen("auth", ({ payload }) => {
+		const { event } = payload;
+		if (event === "autoSignIn") {
+			const user = payload.data;
+			// assign user
+		} else if (event === "autoSignIn_failure") {
+			// redirect to sign in page
+		}
+	});
 }
 
 //Sign in
-export default async function signIn() {
-    try {
-      const user = await Auth.signIn(username, password);
-    } catch (error) {
-      console.log('error signing in', error);
-    }
+export async function signIn() {
+	try {
+		const user = await Auth.signIn(username, password);
+	} catch (error) {
+		console.log("error signing in", error);
+	}
 }
 
 //sign out
-export default async function signOut() {
-    try {
-      await Auth.signOut();
-    } catch (error) {
-      console.log('error signing out: ', error);
-    }
+export async function signOut() {
+	try {
+		await Auth.signOut();
+	} catch (error) {
+		console.log("error signing out: ", error);
+	}
 }
 
 //change password
-export default async function changePassword (oldPassword, newPassword) {
-    try {
-      const user = await Auth.currentAuthenticatedUser();
-      const data = await Auth.changePassword(user, oldPassword, newPassword);
-      console.log(data);
-    } catch(err) {
-      console.log(err);
-    }
-};
+export async function changePassword(oldPassword, newPassword) {
+	try {
+		const user = await Auth.currentAuthenticatedUser();
+		const data = await Auth.changePassword(user, oldPassword, newPassword);
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+}
 
 //Forgot Password
 // Send confirmation code to user's email
-export default async function forgotPassword(username) {
-    try {
-      const data = await Auth.forgotPassword(username);
-      console.log(data);
-    } catch(err) {
-      console.log(err);
-    }
-  } 
-  
+export async function forgotPassword(username) {
+	try {
+		const data = await Auth.forgotPassword(username);
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
 // Collect confirmation code and new password
-export default async function forgotPasswordSubmit(username, code, newPassword) {
-    try {
-      const data = await Auth.forgotPasswordSubmit(username, code, newPassword);
-      console.log(data);
-    } catch(err) {
-      console.log(err);
-    }
+export async function forgotPasswordSubmit(username, code, newPassword) {
+	try {
+		const data = await Auth.forgotPasswordSubmit(username, code, newPassword);
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 //Retrieve current authenticated user
-export default async function currentAuthenticatedUser() {
-    try {
-      const user = Auth.currentAuthenticatedUser({
-        bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-      });
-    } catch(err) {
-      console.log(err);
-    }
-};
-
-
+export async function currentAuthenticatedUser() {
+	try {
+		const user = Auth.currentAuthenticatedUser({
+			bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+		});
+	} catch (err) {
+		console.log(err);
+	}
+}
